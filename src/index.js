b16240addc8d82c55c90408f6b647c97ba5c146e
sairaj <sairaj2119@gmail.com>
2021-06-21 18:43:52 +0530

    add some html to deply to vercel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import attack from './attack';
 
-console.log(attack('Smelly Ogre', 23));
+const resultTag = document.querySelector('#resultTag');
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+
+resultButton.addEventListener('click', () => {
+  console.log('clicked');
+  const result = attack(creature.value, damage.value);
+  resultTag.innerHTML = result;
+});
